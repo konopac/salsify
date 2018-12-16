2c9deaaf0549358a2883331d09b1f498e333bd3f
truenicfel <dassler@hm.edu>
2018-12-16 14:17:54 +0100

    update sender interface

--- src/edu/hm/networks2/salsify/sender/ISender.java
+++ src/edu/hm/networks2/salsify/sender/ISender.java
@@ -2,7 +2,7 @@ package edu.hm.networks2.salsify.sender;
 
 public interface ISender {
 
-	void sendFrame(byte[] frame);
+	void sendFrame(byte[] frame, int gracePeriod, int frameIndex);
 	
 	void startReceivingAcks();
 	
