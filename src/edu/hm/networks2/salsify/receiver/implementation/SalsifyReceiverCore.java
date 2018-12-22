c71da8a70241f96222ecfde877a32a23a029198f
truenicfel <dassler@hm.edu>
2018-12-22 14:54:35 +0100

    adapted receiver to encode based on states and is now able to show images on screen

--- src/edu/hm/networks2/salsify/receiver/implementation/SalsifyReceiverCore.java
+++ src/edu/hm/networks2/salsify/receiver/implementation/SalsifyReceiverCore.java
@@ -9,37 +9,85 @@ import edu.hm.networks2.salsify.receiver.IReceiver;
 import edu.hm.networks2.salsify.receiver.ISalsifyReceiverCore;
 import edu.hm.networks2.salsify.receiver.IScreen;
 import edu.hm.networks2.salsify.receiver.helper.IReceiverListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.imageio.ImageIO;
 
 public class SalsifyReceiverCore implements ISalsifyReceiverCore, IReceiverListener {
 
-	private final IScreen screen;
-	private final ICodec codec;
-	private final IReceiver receiver;
-	
-	public SalsifyReceiverCore() {
-		screen = new Screen();
-		codec = new Codec();
-		receiver = new Receiver();
-	}
-	
-	@Override
-	public void start() {
-		screen.start();
-		receiver.register(this);
-		receiver.start();
-	}
+    private final IScreen screen;
+    private final ICodec codec;
+    private final IReceiver receiver;
 
-	@Override
-	public void join() throws InterruptedException {
-		receiver.join();
-	}
+    /**
+     * Stores source states which can be used for decoding
+     */
+    private final Map<Integer, BufferedImage> sourceStates;
 
-	@Override
-	public void receiveFrame(byte[] data, int frameIndex, int sourceFrameIndex) {
-        System.out.println("SALSIFY: \t processing frame " + frameIndex + "...");
-		// TODO: store source state and use it here
-		final Optional<BufferedImage> frame = codec.decode(Optional.empty(), data);
-		screen.displayFrame(frame.get());
-	}
+    public SalsifyReceiverCore() {
+        screen = new Screen();
+        codec = new Codec();
+        receiver = new Receiver();
+        sourceStates = new TreeMap<>();
+    }
+
+    @Override
+    public void start() {
+        screen.start();
+        receiver.register(this);
+        receiver.start();
+    }
+
+    @Override
+    public void join() throws InterruptedException {
+        receiver.join();
+    }
+
+    @Override
+    public void receiveFrame(byte[] data, int frameIndex, int sourceFrameIndex) {
+        System.out.println("SALSIFY: \t processing frame " + frameIndex + " based on source with index " + sourceFrameIndex + "...");
+
+        Optional<BufferedImage> sourceState;
+        // has no source state?
+        if (sourceFrameIndex == -1) {
+            sourceState = Optional.empty();
+        } else if (sourceStates.containsKey(sourceFrameIndex)) {
+            sourceState = Optional.of(sourceStates.get(sourceFrameIndex));
+        } else {
+            System.out.println("SALSIFY: \t fatal error source frame index (" + sourceFrameIndex + ")  is not stored!");
+            sourceState = Optional.empty();
+        }
+
+        final Optional<BufferedImage> frame = codec.decode(sourceState, data);
+        
+        if (frameIndex == 0) {
+            try {
+                PrintWriter out = new PrintWriter("received.txt");
+                out.print(Arrays.toString(data));
+            } catch (FileNotFoundException ex) {
+                System.out.println("failed opening print writer");
+            }
+        }
+
+        if (frame.isPresent()) {
+            screen.displayFrame(frame.get());
+            sourceStates.put(frameIndex, frame.get());
+            try {
+                // retrieve image
+                File outputfile = new File(frameIndex + ".jpg");
+                ImageIO.write(frame.get(), "jpg", outputfile);
+            } catch (IOException exception) {
+                System.out.println("error occured while writing file to disk");
+            }
+        }
+
+    }
 
 }
--- src/edu/hm/networks2/salsify/receiver/implementation/Screen.java
+++ src/edu/hm/networks2/salsify/receiver/implementation/Screen.java
@@ -1,19 +1,41 @@
 package edu.hm.networks2.salsify.receiver.implementation;
 
 import java.awt.image.BufferedImage;
-
+import javax.swing.JApplet;
 import edu.hm.networks2.salsify.receiver.IScreen;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Screen implements IScreen {
 
-	@Override
-	public void start() {
-        System.out.println("SCREEN: \t started");
-	}
+    private final JFrame parent;
+    private final JLabel imageContainer;
 
-	@Override
-	public void displayFrame(BufferedImage frame) {
+    public Screen() {
+        super();
+        this.parent = new JFrame("Salsify Receiver");
+        this.parent.getContentPane().setLayout(new FlowLayout());
+        this.parent.setSize(1280, 720);
+        this.imageContainer = new JLabel();
+        this.imageContainer.setSize(1280, 720);
+        this.parent.getContentPane().add(this.imageContainer);
+    }
+    
+    @Override
+    public void start() {
+        this.parent.setVisible(true);
+    }
+    
+    @Override
+    public void displayFrame(BufferedImage frame) {
         System.out.println("SCREEN: \t displaying frame " + frame);
-	}
+        imageContainer.removeAll();
+        imageContainer.setIcon(new ImageIcon(frame));
+    }
+    
+    
+    
 
 }
